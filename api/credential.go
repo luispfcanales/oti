4f9c43b55d5aba6a29527d2c711b3991f0aa7d86
luispfcanales <luispfcanales@gmail.com>
2023-04-04 18:35:10 -0500

    change function name:creadentials -> credential

--- api/credential.go
+++ api/credential.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 )
 
-// Credentials is handler that return access to course
-func Credentials(w http.ResponseWriter, r *http.Request) {
+// Credential is handler that return access to course
+func Credential(w http.ResponseWriter, r *http.Request) {
 	c := newCourse()
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(&c)
