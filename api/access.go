7d48afef5292dd53493e1ba81118ff547174f0c4
luispfcanales <luispfcanales@gmail.com>
2023-04-04 18:39:21 -0500

    change name file

--- api/access.go
+++ api/access.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 )
 
-// Credential is handler that return access to course
-func Credential(w http.ResponseWriter, r *http.Request) {
+// Access is handler that return access to course
+func Access(w http.ResponseWriter, r *http.Request) {
 	c := newCourse()
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(&c)
